0da6f650c5954fb727b287724287dc36c8dc0229
chamara <chamara_sachintha@yahoo.com>
2024-10-25 17:03:59 +1100

    Controller integration test

---
+++ Restaurants.API.Tests/Controllers/RestaurantsControllerTests.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+
+namespace Restaurants.API.Controllers.Tests;
+
+public class RestaurantsControllerTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public RestaurantsControllerTests(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact()]
+    public async Task GetAll_ForValidRequest_Return200Ok()
+    {
+        // Arrange
+
+        var client = _factory.CreateClient();
+
+        // Act
+
+        var result = await client.GetAsync("/api/restaurants?pageNumber=1&pageSize=10");
+
+        // Assert
+
+        result.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+    }
+
+    [Fact()]
+    public async Task GetAll_ForInValidRequest_Return400BadRequst()
+    {
+        // Arrange
+
+        var client = _factory.CreateClient();
+
+        // Act
+
+        var result = await client.GetAsync("/api/restaurants");
+
+        // Assert
+
+        result.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
+    }
+}
