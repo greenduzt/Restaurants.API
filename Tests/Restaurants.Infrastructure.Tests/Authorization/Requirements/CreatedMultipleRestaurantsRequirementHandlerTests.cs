f6c53adad7741ed267c781abc430731ef833c007
chamara <chamara_sachintha@yahoo.com>
2025-01-21 17:07:28 +1100

    Restaurants.API project reference updated

--- Tests/Restaurants.Infrastructure.Tests/Authorization/Requirements/CreatedMultipleRestaurantsRequirementHandlerTests.cs
+++ Tests/Restaurants.Infrastructure.Tests/Authorization/Requirements/CreatedMultipleRestaurantsRequirementHandlerTests.cs
@@ -55,41 +55,41 @@ public class CreatedMultipleRestaurantsRequirementHandlerTests
     {
         // arrange
 
-        //var currentUser = new CurrentUser("1", "test@test.com", [], null, null);
-        //var userContextMock = new Mock<IUserContext>();
-        //userContextMock.Setup(m => m.GetCurrentUser()).Returns(currentUser);
+        var currentUser = new CurrentUser("1", "test@test.com", [], null, null);
+        var userContextMock = new Mock<IUserContext>();
+        userContextMock.Setup(m => m.GetCurrentUser()).Returns(currentUser);
 
-        //var restaurants = new List<Restaurant>()
-        //{
-        //    new()
-        //    {
-        //        OwnerId = currentUser.Id,
-        //    },
-        //    new()
-        //    {
-        //        OwnerId = currentUser.Id,
-        //    },
-        //    new()
-        //    {
-        //        OwnerId = "2",
-        //    },
-        //};
+        var restaurants = new List<Restaurant>()
+        {
+            new()
+            {
+                OwnerId = currentUser.Id,
+            },
+            new()
+            {
+                OwnerId = currentUser.Id,
+            },
+            new()
+            {
+                OwnerId = "2",
+            },
+        };
 
-        //var restaurantsRepositoryMock = new Mock<IRestaurantsRepository>();
-        //restaurantsRepositoryMock.Setup(r => r.GetAllAsync()).ReturnsAsync(restaurants);
+        var restaurantsRepositoryMock = new Mock<IRestaurantsRepository>();
+        restaurantsRepositoryMock.Setup(r => r.GetAllAsync()).ReturnsAsync(restaurants);
 
-        //var requirement = new CreatedMultipleRestaurantsRequirement(2);
-        //var handler = new CreatedMultipleRestaurantsRequirementHandler(restaurantsRepositoryMock.Object,
-        //    userContextMock.Object);
-        //var context = new AuthorizationHandlerContext([requirement], null, null);
+        var requirement = new CreatedMultipleRestaurantsRequirement(2);
+        var handler = new CreatedMultipleRestaurantsRequirementHandler(restaurantsRepositoryMock.Object,
+            userContextMock.Object);
+        var context = new AuthorizationHandlerContext([requirement], null, null);
 
-        //// act
+        // act
 
-        //await handler.HandleAsync(context);
+        await handler.HandleAsync(context);
         
-        //// assert
+        // assert
         
-        //context.HasSucceeded.Should().BeTrue();
+        context.HasSucceeded.Should().BeTrue();
 
     }
 }
